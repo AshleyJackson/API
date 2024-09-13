16259113378ef9800f99487308ca62a719cf9349
Ashley Jackson <Ashley@AshleyJackson.net>
2024-09-13 20:19:55 +1000

    revert

--- packages/functions/src/email/validate.ts
+++ packages/functions/src/email/validate.ts
@@ -2,71 +2,50 @@ import { ApiHandler } from "sst/node/api";
 import { validate } from 'deep-email-validator'
 
 type output = {
-    valid: boolean,
-    validators: {
-        regex: {
-            valid: boolean,
-        },
-        typo: {
-            valid: boolean,
-        },
-        disposable: {
-            valid: boolean,
-        },
-        mx: {
-            valid: boolean,
-            note: string,
-        },
-        smtp: {
-            valid: boolean,
-            note: string,
-        },
-    }
+	valid: boolean,
+	validators: {
+		regex: {
+			valid: boolean,
+		},
+		typo: {
+			valid: boolean,
+		},
+		disposable: {
+			valid: boolean,
+		},
+		mx: {
+			valid: boolean,
+			note: string,
+		},
+		smtp: {
+			valid: boolean,
+			note: string,
+		},
+	}
 }
 
 export const handler = ApiHandler(async (_evt) => {
-    let output: output
-    const email = _evt.pathParameters?.email;
-    if (!email) {
-        return {
-            statusCode: 400,
-            body: JSON.stringify({
-                error: 'Missing email',
-            }),
-        };
-    }
+	let output: output
+	const email = _evt.pathParameters?.email;
+	if (!email) {
+		return {
+			statusCode: 400,
+			body: JSON.stringify({
+				error: 'Missing email',
+			}),
+		};
+	}
 
-    console.log(`Validating email: ${email}`);
-    const request: any = await validate({
-        email: email,
-        validateMx: false, // Note: Disabled for now, as it requires the function to have internet access
-        validateSMTP: false, // Note: Disabled for now, as it requires the function to have internet access
-    });
-    console.log(`Validation result: ${JSON.stringify(request)}`);
-    output = {
-        valid: request.valid,
-        validators: {
-            regex: {
-                valid: request.validators.regex.valid,
-            },
-            typo: {
-                valid: request.validators.typo.valid,
-            },
-            disposable: {
-                valid: request.validators.disposable.valid,
-            },
-            mx: {
-                valid: request.validators.mx.valid,
-                note: "Disabled as of 2024-09-01",
-            },
-            smtp: {
-                valid: request.validators.smtp.valid,
-                note: "Disabled as of 2024-09-01",
-            },
-        }
-    }
-    return {
-        statusCode: 200,
-        body: output,
-    };
+	console.log(`Validating email: ${email}`);
+	const request = await validate({
+		email: email,
+		validateMx: false, // Note: Disabled for now, as it requires the function to have internet access
+		validateSMTP: false, // Note: Disabled for now, as it requires the function to have internet access
+	});
+
+	console.log(`Validation result: ${JSON.stringify(request)}`);
+	return {
+		statusCode: 200,
+		body: request,
+	};
 })
