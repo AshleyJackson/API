3972793a99d28ae3f99ed2924bd58f203a1c0f5b
Ashley Jackson <Ashley@AshleyJackson.net>
2024-09-17 08:35:41 +1000

    Path

--- stacks/API.ts
+++ stacks/API.ts
@@ -4,22 +4,23 @@ import { StackContext, Api } from 'sst/constructs'
 const certArn = process.env['CERT_ARN'] as string
 
 export function API({ stack }: StackContext) {
-    const api = new Api(stack, 'Api', {
-        routes: {
-            'GET /': 'packages/functions/src/home.handler',
-            'GET /imdb/list/{id}': 'packages/functions/src/imdb/list.handler',
-            'GET /imdb/title/{id}': 'packages/functions/src/imdb/title.handler',
-            'GET /email/validate/{email}': 'packages/functions/src/email/validate.handler',
-        },
-        customDomain: {
-            domainName: 'api.ashleyjackson.net',
-            isExternalDomain: true,
-            cdk: {
-                certificate: Certificate.fromCertificateArn(stack, 'Certificate', certArn),
-            }
-        },
-    });
-    stack.addOutputs({
-        ApiEndpoint: api.url,
-    })
+	const api = new Api(stack, 'Api', {
+		routes: {
+			'GET /': 'packages/functions/src/home.handler',
+			'GET /imdb/list/{id}': 'packages/functions/src/imdb/list.handler',
+			'GET /imdb/title/{id}': 'packages/functions/src/imdb/title.handler',
+			'GET /email/validate/{email}': 'packages/functions/src/email/validate.handler',
+		},
+		customDomain: {
+			path: '',
+			domainName: 'api.ashleyjackson.net',
+			isExternalDomain: true,
+			cdk: {
+				certificate: Certificate.fromCertificateArn(stack, 'Certificate', certArn),
+			}
+		},
+	});
+	stack.addOutputs({
+		ApiEndpoint: api.url,
+	})
 }
