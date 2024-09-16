3448cf6b361d4a8fd4d04e12781200e973bc3dd5
Ashley Jackson <Ashley@AshleyJackson.net>
2024-09-16 14:33:43 +1000

    Bun and try catch

--- packages/functions/src/email/validate.ts
+++ packages/functions/src/email/validate.ts
@@ -1,31 +1,7 @@
 import { ApiHandler } from "sst/node/api";
 import { validate } from 'deep-email-validator'
 
-type output = {
-	valid: boolean,
-	validators: {
-		regex: {
-			valid: boolean,
-		},
-		typo: {
-			valid: boolean,
-		},
-		disposable: {
-			valid: boolean,
-		},
-		mx: {
-			valid: boolean,
-			note: string,
-		},
-		smtp: {
-			valid: boolean,
-			note: string,
-		},
-	}
-}
-
 export const handler = ApiHandler(async (_evt) => {
-	let output: output
 	const email = _evt.pathParameters?.email;
 	if (!email) {
 		return {
@@ -37,11 +13,22 @@ export const handler = ApiHandler(async (_evt) => {
 	}
 
 	console.log(`Validating email: ${email}`);
-	const request = await validate({
-		email: email,
-		validateMx: false, // Note: Disabled for now, as it requires the function to have internet access
-		validateSMTP: false, // Note: Disabled for now, as it requires the function to have internet access
-	});
+	let request
+	try {
+		request = await validate({
+			email: email,
+			// validateMx: false,
+			// validateSMTP: false,
+		});
+	} catch (error) {
+		console.error(`Validation error: ${error}`);
+		return {
+			statusCode: 400,
+			body: JSON.stringify({
+				error: 'Invalid email',
+			}),
+		};
+	}
 
 	console.log(`Validation result: ${JSON.stringify(request)}`);
 	return {
